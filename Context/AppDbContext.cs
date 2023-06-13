941f8ebe8efab38845141d0666d604badc1593c9
Nathan Faria <nathanfaria607@gmail.com>
2023-06-13 13:58:22 -0300

    feat: add AppDbContext

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using JWT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
