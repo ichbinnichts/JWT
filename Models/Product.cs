aa28805e943cdec0fde6cd539ea13f03e2001565
Nathan Faria <nathanfaria607@gmail.com>
2023-06-13 13:58:04 -0300

    feat: add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JWT.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string? Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+    }
+}
